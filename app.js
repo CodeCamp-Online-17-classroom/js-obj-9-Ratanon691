5caa08e5f7c0407778b6ed55d3237383072137dc
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-25 22:11:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// write code here
+function createObj() {
+  let obj = {};
+  while (true) {
+    let key = prompt(`Enter a key`);
+    if (key.toLowerCase() === `stop`) {
+      break;
+    }
+    let value = prompt(`Enter a value`);
+    obj[key] = value;
+  }
+  console.log(obj);
+}
+
